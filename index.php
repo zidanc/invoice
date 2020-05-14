9f02c6faa3bff57b07acc97e18ac379da4574dd8
Zidan <b93605079@g.ntu.edu.tw>
2020-05-14 11:50:56 +0800

    發票清單列表的查詢與顯示當前月該如何寫

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include "./common/base.php"?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- list.php
+++ list.php
@@ -1,3 +1,4 @@
+<?php include "./common/base.php"?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +8,58 @@
   <link rel="stylesheet" href=".css/style.css">
 </head>
 <body>
-  <?php include "./include/header.php";?>
+  <?php include "./include/header.php";
+  $month=date("n");     //不用m而用n，是因為我們不想要有前導0，有可能會因為回傳是字串而出現問題。
+  $period=round(($month/2),0);
+  echo "目前月份所在期數:"."&nbsp;" . "$period" . "期";
+  
+  if(isset($_GET["period"])){
+    $period=$_GET["period"];
+  }
+  ?>
+  
+  <h1>發票列表</h1>
+  <ul class="nav">
+    <li><a href="list.php?period=1" style="background:<?=($period==1)?"lightgreen":"white";?>">1(1,2)</a></li>
+    <li><a href="list.php?period=2" style="background:<?=($period==2)?"lightgreen":"white";?>">2(3,4)</a></li>
+    <li><a href="list.php?period=3" style="background:<?=($period==3)?"lightgreen":"white";?>">3(5,6)</a></li>
+    <li><a href="list.php?period=4" style="background:<?=($period==4)?"lightgreen":"white";?>">4(7,8)</a></li>
+    <li><a href="list.php?period=5" style="background:<?=($period==5)?"lightgreen":"white";?>">5(9,10)</a></li>
+    <li><a href="list.php?period=6" style="background:<?=($period==6)?"lightgreen":"white";?>">6(11,12)</a></li>
+  </ul>
+
+  <?php
+  
+    $sql="select * from `invoice` where period='$period'";
+    $rows=$pdo->query($sql)->fetchAll();
+  ?>
+  
+  <table>
+    <tr>
+      <td>編號</td>
+      <td>標記</td>
+      <td>號碼</td>
+      <td>花費</td>
+    </tr>
+    
+  
+  
+  <?php
+  foreach ($rows as $row) {
+  ?>  
+  
+  <tr>
+      <td><?=$row['id'];?></td>
+      <td><?=$row['code'];?></td>
+      <td><?=$row['number'];?></td>
+      <td><?=$row['expend'];?></td>
+  </tr>
+   
+  
+
+  <?php
+  }
+  ?> 
+</table>
 </body>
 </html>
