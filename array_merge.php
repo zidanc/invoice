7a59374998f6001535e2dd55886c072deaba0ecf
Zidan <b93605079@g.ntu.edu.tw>
2020-05-15 15:11:36 +0800

    利用自訂函式做sql的插入語法

--- array_merge.php
+++ array_merge.php
@@ -13,9 +13,9 @@ echo "<pre>";print_r($c);"</pre>";
 <?php
 
 // 將上述一個個增加的方式，這邊設計一個不用一個個增加的自訂function，讓此自訂函式功能等同於系統提供的array_merge()
-function arr_mer($a,...$b){
+function arr_mer($a,...$b){     //這種寫法的$b就已經是二維陣列了。
     if(is_array($a)){
-      $array=$a;
+      $array=$a;        //所以這時候陣列array index=0已經是$a陣列了。$array不寫中括號就讓$a丟進去，這樣$array此時是帶有陣列的變數。用echo就可以顯示出來，不用到print_r。
     }else{
       echo "not an array";
     }
