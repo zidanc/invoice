20971e8423801ca873f8094e84f8ca4eb45b4026
zidanc <zidanc@hotmail.com>
2020-06-07 21:20:49 +0800

    沒有第二組where語句，要用空字串的原因。all自定函式

--- practice/all_pra.php
+++ practice/all_pra.php
@@ -17,7 +17,11 @@ function select($table,...$arg){        //,... 這樣子若函式只有$table此
     
     $sql=$sql." where ".join(" && ",$tmp);
   }
-  // echo $sql;
+
+  if(isset($arg[1])){
+    $sql=$sql. $arg[1];
+  }
+  echo $sql."<br>";
   return $pdo->query($sql)->fetchAll();
 }
 
@@ -43,5 +47,30 @@ foreach($rows as $row){
   echo $row['expend'];
   echo "<hr>";
 }
+
+echo "<p>有帶兩組以上的參數，第三組是限制條件中的字串</p>";
+$rows=select("invoice",["period"=>'2',"year"=>'2020']," Order by `id` DESC");
+
+foreach($rows as $row){
+  echo $row['id']."-";
+  echo $row['code']."-";
+  echo $row['number']."-";
+  echo $row['period']."-";
+  echo $row['expend'];
+  echo "<hr>";
+}
+
+echo "<p>有帶兩組以上的參數，沒有第二組where語句</p>";
+// $rows=select("invoice",[]," Order by `id` DESC");    //第二組寫空陣列會出問題，它的$sql:select * from invoice where Order by `id` DESC
+$rows=select("invoice",""," Order by `id` DESC");  //我們要讓它完全沒有機會去判斷它是個陣列，所以直接用雙引號，空字串。
+foreach($rows as $row){
+  echo $row['id']."-";
+  echo $row['code']."-";
+  echo $row['number']."-";
+  echo $row['period']."-";
+  echo $row['expend'];
+  echo "<hr>";
+}
+
 
 ?>
