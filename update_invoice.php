8eff6da601a23ca724557c9c4815f3b03a68018b
Zidan <b93605079@g.ntu.edu.tw>
2020-06-19 13:27:41 +0800

    針對遺漏頁面外觀統整

--- update_invoice.php
+++ update_invoice.php
@@ -1,4 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>統一發票管理系統</title>
+  <link rel="stylesheet" href="./css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+</head>
+<body>
 <?php 
+
+echo "<div class='con opt bor'>";
 
 include "./common/base.php";
 // echo "<pre>";print_r($_POST);"</pre>";
@@ -10,4 +22,11 @@ $res= save('invoice',$_POST);
     echo "更新失敗 / 沒有影響任何筆資料";
   }
 
+echo "</div>"
+
 ?>
+
+
+  
+</body>
+</html>
